8be0c61c4836da34a3232cac7b3af5e1d4383870
CesarRawr <rawrcesar13@gmail.com>
2022-10-23 20:35:24 -0500

    Poniendo estructura y moviendo los ejemplos a una carpeta para ejemplos

---
+++ lista_prestamos.php
@@ -0,0 +1,64 @@
+<?php
+
+  
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Lista de préstamos</title>
+
+  <!-- Google Icons -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+  <!-- Google Roboto Font -->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+  <!-- Misc css -->
+  <link rel="stylesheet" type="text/css" href="public/css/reset.css">
+  <link rel="stylesheet" type="text/css" href="public/css/lista-prestamos/lista-prestamos.css">
+  <link rel="stylesheet" type="text/css" href="public/css/lista-prestamos/header.css">
+</head>
+<body>
+  <div class="container">
+    <header>
+      <nav class="hamburger-menu">
+        <span class="material-symbols-outlined md">menu</span>
+        <ul>
+          <li>
+            <a href="#">
+              <span>Opción 1</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>Opción 2</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>Opción 3</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <span class="logo">Sistema de préstamos</span>
+
+      <div class="user">
+        <span class="material-symbols-outlined md">person</span>
+      </div>
+    </header>
+
+    <main>
+      principal
+    </main>
+  </div>
+</body>
+</html>
+
