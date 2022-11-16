f6cc310440aac07571a80143562590f52d7f2f4f
Roberto-ParGon <89487643+Roberto-ParGon@users.noreply.github.com>
2022-11-15 18:00:14 -0600

    Update lista_dispositivos.php

--- lista_dispositivos.php
+++ lista_dispositivos.php
@@ -1,10 +1,8 @@
 <?php
-  session_start();
   include_once('public/php/lista-dispositivos/DispositivosController.php');
 
   $controller = new DispositivosController();
   $dispositivos = $controller->getDispositivosInfo();
-  $_SESSION['DISPOSITIVO']
 ?>
 
 <!DOCTYPE html>
@@ -109,13 +107,6 @@
                 <td><?= $dispositivo['cantidad'] ?> </td>
                 <td><?= $dispositivo['observaciones'] ?> </td>
                 <td>
-<!-- Enviar el formulario a otro documento con $_GET
-
- -->
-
-                  <a href="<?=>">
-                    
-                  </a>
                   <span class="material-symbols-outlined f">
                     edit_square
                   </span>
